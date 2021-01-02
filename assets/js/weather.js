fcf45e829891b7c3cd929b99804b0f1238d2fb8f
limunosekai <powerlsh0103@gmail.com>
2021-01-02 15:20:13 +0900

    End weather.js 날씨 기능 추가

---
+++ assets/js/weather.js
@@ -0,0 +1,56 @@
+const weather = document.querySelector('.weather');
+const weatherData = weather.querySelector('h3');
+
+const COORDS = 'coords';
+const API_KEY = '147ae65bb2a104c347e0c9634023954d';
+
+function getWeather(lati, longi) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&appid=${API_KEY}&units=metric`)
+  .then(response => {
+    return response.json()
+  }).then(function(json) {
+    console.log(json);
+    const temp = json.main.temp;
+    const weatherCondition = json.weather[0].description;
+    weatherData.innerText = `${temp} °C ${weatherCondition}`
+  });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoError() {
+  alert("No access");
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if(loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
